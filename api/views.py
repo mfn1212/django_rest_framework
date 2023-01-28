cc7f781599bc3c7930918b1c1464088bc29bdb6e
Mehran <mehranfo12@gmail.com>
2023-01-28 10:45:41 +0330

    create first api view and configuring its url

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def api_home(request, *args, **kwargs):
+    json_data = {
+        'massage': "hi here is my first api"
+    }
+    return JsonResponse(json_data)
+
