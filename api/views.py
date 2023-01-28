c672f04aeb97911712bdc9f3459c9dc1c0763200
Mehran <mehranfo12@gmail.com>
2023-01-28 13:39:11 +0330

    reading data from model (without serializer)

--- api/views.py
+++ api/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    json_data = {
-        'massage': "hi here is my first api"
-    }
-    return JsonResponse(json_data)
-
+    # json_data = {
+    #     'massage': "hi here is my first api"
+    # }
+    # return JsonResponse(json_data)
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data['title'] = model_data.title 
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+    return JsonResponse(data)    
