a47d1729122628a12433a12194fc10ddaa76060b
Mehran <mehranfo12@gmail.com>
2023-01-28 17:55:12 +0330

    using rest_framework response

--- api/views.py
+++ api/views.py
@@ -2,8 +2,9 @@ from django.forms import model_to_dict
 from django.shortcuts import render
 from django.http import JsonResponse
 from products.models import Product
-
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+@api_view(['GET','POST'])
 def api_home(request, *args, **kwargs):
     # json_data = {
     #     'massage': "hi here is my first api"
@@ -12,9 +13,10 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        # data['id'] = model_data.id 
-        # data['title'] = model_data.title 
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
         # data['content'] = model_data.content
         # data['price'] = model_data.price
-        data = model_to_dict(model_data,fields=['id','title','content','price'])
-    return JsonResponse(data)    
+        data = model_to_dict(model_data, fields=[
+                             'id', 'title', 'content', 'price'])
+    return Response(data)
