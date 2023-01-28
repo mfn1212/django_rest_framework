e0d83f55f5d77c29359af440dd51a08f0115de79
Mehran <mehranfo12@gmail.com>
2023-01-28 13:53:55 +0330

    api read from model with model_to_dict

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from django.http import JsonResponse
 from products.models import Product
@@ -11,7 +12,9 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title'] = model_data.title 
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        # data['id'] = model_data.id 
+        # data['title'] = model_data.title 
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
+        data = model_to_dict(model_data,fields=['id','title','content','price'])
     return JsonResponse(data)    
