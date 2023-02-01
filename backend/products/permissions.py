913b34564a39e08e95ea2d58671d87b6870a48ae
Mehran <mehranfo12@gmail.com>
2023-02-01 12:50:59 +0330

    custom permission overriding perms_map

--- backend/products/permissions.py
+++ backend/products/permissions.py
@@ -19,18 +19,17 @@ class IsOunerOrReadOnly(permissions.BasePermission):
 
 class IsStuffEditorPermission(permissions.DjangoModelPermissions):
 
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
     def has_permission(self, request, view):
-        """
-        if we have any permissin retern true 
-        """
-        user = request.user
-        if user.is_staff:
-            if user.has_perm("products_add_product"):
-                return True
-            if user.has_perm("products_delete_product"):
-                return True
-            if user.has_perm("products_change_product"):
-                return True
-            if user.has_perm("products_view_product"):
-                return True
-        return False
+        if not request.user.is_staff:
+            return False
+        return super().has_permission(request, view)
